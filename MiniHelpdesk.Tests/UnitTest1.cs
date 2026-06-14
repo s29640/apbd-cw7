2c8a20296d9655bce83f5aeb42ca98fa00bb9f7b
Robert Sendys <s29640@pjwstk.edu.pl>
2026-06-14 11:36:03 +0200

    Inicjuj rozwiązanie i strukturę projektu

---
+++ MiniHelpdesk.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniHelpdesk.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ MiniHelpdesk/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiniHelpdesk.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
